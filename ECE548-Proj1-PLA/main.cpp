90899e064746546ddbf4027ca62eb60272fe967d
lia-murphy564 <wxm219@miami.edu>
2021-09-15 20:22:58 -0400

    Add project files.

---
+++ ECE548-Proj1-PLA/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	
+}
---
+++ ECE548-Proj1-PLA/model.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <vector>
+
+#include "model.h"
+
+using namespace std;
+
+class PLA
+{
+protected:
+    //vector<attribute> attributes;
+
+    double bias;
+    double rate;
+
+public:
+    void UpdateWeights()
+    {
+
+    }
+};
